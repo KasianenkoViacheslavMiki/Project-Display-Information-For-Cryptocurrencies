81c6161c2a7fc03a95e179dc5e3aa8a1f618c84c
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-05-26 22:55:00 +0300

    Implementation user interface

---
+++ Project-Display-Information-For-Cryptocurrencies/ViewModels/NavigationVerticalBarViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Project_Display_Information_For_Cryptocurrencies.Commands.UICommands;
+using Project_Display_Information_For_Cryptocurrencies.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Project_Display_Information_For_Cryptocurrencies.ViewModels
+{
+    public class NavigationVerticalBarViewModel:BaseViewModel
+    {
+        public NavigationVerticalBarViewModel()
+        {
+            HomeCommand = new NavigationCommand<HomeViewModel>(NavigationStore.GetInstance() ,()=>new HomeViewModel());
+            SettingsCommand = new NavigationCommand<SettingsViewModel>(NavigationStore.GetInstance(), () => new SettingsViewModel());
+        }
+
+        public ICommand HomeCommand{ get;}
+        public ICommand SettingsCommand { get; }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+    }
+}
