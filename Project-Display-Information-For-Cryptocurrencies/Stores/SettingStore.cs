ad3c0980d47e2085d5b41c45cb0b4f4d4e3d6bdf
Kasianenko Viacheslav <55177392+KasianenkoViacheslavMiki@users.noreply.github.com>
2023-05-30 05:10:22 +0300

    Amator UI design with toggle theme

---
+++ Project-Display-Information-For-Cryptocurrencies/Stores/SettingStore.cs
@@ -0,0 +1,42 @@
+﻿using Project_Display_Information_For_Cryptocurrencies.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Display_Information_For_Cryptocurrencies.Stores
+{
+    public class SettingStore
+    {
+        //False = dark | True = ligth
+        private bool toogleTheme = true;
+
+
+        private static SettingStore instance;
+        private static SettingStore Instance
+        {
+            get => instance;
+            set => instance = value;
+        }
+        public bool ToogleTheme 
+        { 
+            get => toogleTheme; 
+            set => toogleTheme = value; 
+        }
+
+        private SettingStore() 
+        { 
+
+        }
+        public static SettingStore GetInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new SettingStore();
+            }
+            return Instance;
+        }
+    }
+}
--- Project-Display-Information-For-Cryptocurrencies/ViewModels/BaseViewModel.cs
+++ Project-Display-Information-For-Cryptocurrencies/ViewModels/BaseViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_Display_Information_For_Cryptocurrencies.Service;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -9,6 +10,36 @@ namespace Project_Display_Information_For_Cryptocurrencies.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged, IDisposable
     {
+        protected ServiceSetting serviceSetting;
+
+        public BaseViewModel()
+        {
+            this.serviceSetting = ServiceSetting.GetInstance()
+                                                .SettingInstance(OnChangeTheme);
+        }
+
+        public void OnChangeTheme()
+        {
+            OnPropertyChanged(nameof(ButtonColor));
+            OnPropertyChanged(nameof(BackgroundColor));
+            OnPropertyChanged(nameof(ForegroundColor));
+            OnPropertyChanged(nameof(Toogle));
+        }
+        public bool Toogle
+        {
+            get
+            {
+                return serviceSetting.Toggle;
+            }
+            set
+            {
+                serviceSetting.Toggle = value;
+            }
+        }
+        public string ButtonColor => serviceSetting.ButtonColor;
+        public string BackgroundColor => serviceSetting.BackgroundColor;
+        public string ForegroundColor => serviceSetting.ForegroundColor;
+
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void OnPropertyChanged(string propertyName)
